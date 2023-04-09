432d8b3d0d1703dd1a1a31e21a6d09f19a7bb787
Wadii Hajji <wadii@cardiologs.com>
2023-04-09 18:32:29 +0200

    fix: Replace path with contents

---
+++ Migrations/20230409160529_ReplacePathWithContents.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace colle.Migrations
+{
+    public partial class ReplacePathWithContents : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "path",
+                table: "pastes",
+                newName: "contents");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "contents",
+                table: "pastes",
+                newName: "path");
+        }
+    }
+}
